abd133387f537c09789c4a5396e894b8f5bb2524
Antoniodigiovanni <antoniodg98@gmail.com>
2021-06-01 18:25:59 +0200

    Correct MinVar (not complete)

--- MinVar.R
+++ MinVar.R
@@ -1,4 +1,9 @@
-# Do not run the entire script! It requires a Matlab script before the second for loop can be executed
+################################################################################
+#                                                                              #
+# Do not run the entire script! It requires a Matlab script before the second  #
+# loop can be executed, run until line 54 and then move to Matlab              #
+#                                                                              #
+################################################################################
 
 library(rstudioapi)
 
@@ -10,7 +15,8 @@ source("Real_data_prep.R")
 source("FAT_RM.R")
 
 # Reconstruction date -> July of each year
-# We select the first N=1000 stocks for each year and calculate the covariance (less than 1000 present)
+# We select the first N=1000 stocks for each year and calculate the covariance 
+# (less than 1000 stocks present on average, so we take all of them)
 # matrix based on the excess returns of the previous 36 months (60 in the paper)  excess returns
 
 # Stocks selection
@@ -75,8 +81,8 @@ for (y in Years_list){
     # Perform calculations on portfolios or create some df with all the Ids and the
     # weights for each year ...
     stocks_ret <- stocks %>% 
-      filter(ym < as.yearmon(paste0("Jul", year(as.yearmon(y)))) &
-               ym >= (as.yearmon(paste0("Jul", year(as.yearmon(y))))-36/12))
+      filter(ym >= as.yearmon(paste0("Jul", year(as.yearmon(y)))) &
+               ym < (as.yearmon(paste0("Jul", year(as.yearmon(y))))+1))
     stocks_ret <- stocks_ret %>% filter(Id %in% Weight$Id) %>% mutate(RET = (RET/100+1)) %>% 
       group_by(Id) %>% 
       summarise(Ret = prod(RET)) %>% mutate(Ret = Ret-1)
@@ -89,8 +95,36 @@ for (y in Years_list){
 }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
 
 }
-# Calculate portfolio returns
-Portfolio_Returns <- Portfolio_Returns %>% arrange(y) %>% 
-  mutate(ret = 1+portfolio_ret, Value=100) %>% mutate(Value = lag(Value) *ret)
-Portfolio_Returns$Value[1] <- 100
-plot(Portfolio_Returns$y, Portfolio_Returns$Value, type = "l")
+# Calculate portfolio return
+# Check Calculation ...
+# Portfolio_Returns <- Portfolio_Returns %>% arrange(y) %>% 
+#   mutate(ret = 1+portfolio_ret, Value=100) %>% mutate(cumproduct = cumprod(ret)) %>% mutate(Value = lag(Value)*lag(cumproduct))
+# Portfolio_Returns$Value[1] <- 100
+
+Portfolio_Returns <- Portfolio_Returns %>% arrange(y) %>%
+  mutate(ret = 1+portfolio_ret) %>% mutate(Portfolio_Value = 100*lag(cumprod(ret)))
+Portfolio_Returns$Portfolio_Value[1] <- 100
+
+# Calculate Market Portfolio Return for the same time-frane
+source("FAT_RM.R")
+Market_Portfolio_FAT <- as.data.table(Market_Portfolio_FAT)
+Market_Portfolio_FAT[,y:=year(ym)]
+Market_Portfolio_FAT[,m := month(ym)]
+Market_Portfolio_FAT <- Market_Portfolio_FAT %>% filter(ym>="Jul 1995")
+Market_Portfolio_FAT[, hcjun := ifelse(m>=7, y, y-1)]
+
+Market_Ret <- Market_Portfolio_FAT %>% mutate(RM = (RM/100+1)) %>% 
+  group_by(hcjun) %>% summarise(Market_Ret = prod(RM)) %>% 
+  mutate(Market_Ret = Market_Ret-1) %>% rename("y" = hcjun)
+Market_Ret <- Market_Ret %>% arrange(y) %>% 
+  mutate(ret = 1+Market_Ret) %>% mutate(Market_Portfolio = 100*lag(cumprod(ret)))
+Market_Ret$Market_Portfolio[1] <- 100
+Market_Ret <- Market_Ret %>% select(y, Value_Market)
+Portfolio_Returns <- left_join(Portfolio_Returns,
+                               Market_Ret,
+                               by = "y")
+
+
+# Plot Minimum Variance against the Market Return
+plot(x = Portfolio_Returns$y, y = Portfolio_Returns$Portfolio_Value, type = "l", lty=1, ylim=c(50,17000))
+lines(x = Portfolio_Returns$y, y = Portfolio_Returns$Market_Portfolio, lty=2)
